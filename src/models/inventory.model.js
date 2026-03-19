f6925c1c9e55a5cc17bfdd77cdbd00efd6c55af0
elorajm <eloraathias@gmail.com>
2026-03-19 15:46:03 -0500

    Images

--- src/models/inventory.model.js
+++ src/models/inventory.model.js
@@ -2,10 +2,17 @@ import db from "./db.js";
 
 export async function getAllVehicles() {
   const result = await db.query(`
-    SELECT v.*, c.name AS category
+    SELECT 
+      v.*,
+      c.name AS category,
+      vi.image_url,
+      vi.alt_text
     FROM vehicles v
     LEFT JOIN categories c
-    ON v.category_id = c.category_id
+      ON v.category_id = c.category_id
+    LEFT JOIN vehicle_images vi
+      ON v.vehicle_id = vi.vehicle_id
+      AND vi.is_primary = true
     ORDER BY v.created_at DESC
   `);
 
@@ -15,10 +22,17 @@ export async function getAllVehicles() {
 export async function getVehicleById(vehicleId) {
   const result = await db.query(
     `
-    SELECT v.*, c.name AS category
+    SELECT 
+      v.*,
+      c.name AS category,
+      vi.image_url,
+      vi.alt_text
     FROM vehicles v
     LEFT JOIN categories c
-    ON v.category_id = c.category_id
+      ON v.category_id = c.category_id
+    LEFT JOIN vehicle_images vi
+      ON v.vehicle_id = vi.vehicle_id
+      AND vi.is_primary = true
     WHERE v.vehicle_id = $1
   `,
     [vehicleId]
