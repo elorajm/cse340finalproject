2804212ef8381cb37ebffe6c7c20bd69b63c4cf4
elorajm <eloraathias@gmail.com>
2026-03-19 19:02:22 -0500

    Working on homepage

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,5 +1,16 @@
-export function getHome(req, res) {
-  res.render("index", {
-    title: "Car Dealership",
-  });
+import { getAllVehicles } from "../models/inventory.model.js";
+
+export async function getHome(req, res, next) {
+  try {
+    const all = await getAllVehicles(null, "newest");
+    const featured = all.slice(0, 3);
+
+    res.render("index", {
+      title: "Home",
+      bodyClass: "home-page",
+      featured
+    });
+  } catch (error) {
+    next(error);
+  }
 }
