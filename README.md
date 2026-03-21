2acda129cac6fd0fbb1b625ec56a897b11bfe8f0
elorajm <eloraathias@gmail.com>
2026-03-21 13:40:24 -0500

    Fixing Styling

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 To Do Still
 
-
-
-
-On the owners side they should be able to see the cars that have been added to wish list and which users have it on their wish list. 
-
-when you make a review if you click 4 stars when you upload the review it only says 2 stars. Make sure that is fixed and shows what the reviewer actually clicked.
-
+When You log it it should say youve logged in 
+Get it render ready. 
+Write the README
--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -5,10 +5,16 @@ export async function getHome(req, res, next) {
     const all = await getAllVehicles(null, "newest");
     const featured = all.slice(0, 3);
 
+    // Pick a random vehicle image for the hero on each page load
+    const heroVehicle = all.length > 0
+      ? all[Math.floor(Math.random() * all.length)]
+      : null;
+
     res.render("index", {
       title: "Home",
       bodyClass: "home-page",
-      featured
+      featured,
+      heroVehicle
     });
   } catch (error) {
     next(error);
